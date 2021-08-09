66566659302fc359f6e447a776f3f118f535fcd6
tp86 <tom0palka+tp86@gmail.com>
2021-08-09 17:06:07 +0200

    Step 11: Implement views and render response using templates #5 - error shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-from django.http.response import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from polls.models import Question
 
@@ -12,10 +11,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
